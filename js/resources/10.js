dac3295544a13f08c5a762a8f62add512fcd1cb7
straveski <dasffalcao0@gmail.com>
2022-03-22 23:47:05 +0000

    facil

--- js/resources/10.js
+++ js/resources/10.js
@@ -4,7 +4,7 @@ document.write('\
 		<div class="thumb">\
 			<div class="hover-effect">\
 				<div class="hover-content">\
-					<h1><em>Diogo</em> Falcão</h1>\
+					<h1><em>Diogo </em> Falcão</h1>\
 				</div>\
 			</div>\
 			<div class="image">\
